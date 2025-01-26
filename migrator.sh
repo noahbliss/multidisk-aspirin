52ebf0039fd22f67bb42c20c988f92d96d345ee9
Noah Bliss <noah@superuser.sh>
2025-01-26 19:12:12 +0000

    Update migrator.sh
    added a bunch of double quotes

--- migrator.sh
+++ migrator.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-T_ROOT=/mnt/disk/user
-LINK_LIST=linklist
+T_ROOT="/mnt/disk/user"
+LINK_LIST="linklist"
 
-TARGET=$(realpath $1)
+TARGET=$(realpath "$1")
 
 if ! [[ "$TARGET" =~ ^"$HOME" ]]; then
   echo "Can only migrate directories below $HOME"
@@ -12,9 +12,9 @@ fi
 
 if [ -d "$TARGET" ]; then
   # continue
-  cd $T_ROOT
-  R_TARGET=$(echo $TARGET | sed "s|$HOME/||")
-  mkdir -p $(dirname $R_TARGET)
+  cd "$T_ROOT"
+  R_TARGET=$(echo "$TARGET" | sed "s|$HOME/||")
+  mkdir -p $(dirname "$R_TARGET")
   mv "$TARGET" "$R_TARGET"
   ln -s "$T_ROOT/$R_TARGET" "$TARGET"
   echo "$TARGET" >> "$T_ROOT/$LINK_LIST"
